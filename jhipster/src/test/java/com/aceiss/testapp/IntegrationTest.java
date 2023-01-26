ab574a584fa3f3d78ea8cf035870f959e3f09e68
Eric Baker <ericbaker@aceiss.com>
2023-01-26 12:11:12 -0700

    Initial version of springapp generated by generator-jhipster@7.9.3

---
+++ jhipster/src/test/java/com/aceiss/testapp/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.aceiss.testapp;
+
+import com.aceiss.testapp.SpringappApp;
+import com.aceiss.testapp.config.AsyncSyncConfiguration;
+import com.aceiss.testapp.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { SpringappApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
