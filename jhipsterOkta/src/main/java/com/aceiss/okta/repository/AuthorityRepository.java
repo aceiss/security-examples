1edc693ff4dbf3493383a80df20aa2a718804143
Eric Baker <ericbaker@aceiss.com>
2023-02-02 12:55:12 -0700

    Initial version of jhipsterOkta generated by generator-jhipster@7.9.3

---
+++ jhipsterOkta/src/main/java/com/aceiss/okta/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.aceiss.okta.repository;
+
+import com.aceiss.okta.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ jhipsterOkta/src/main/java/com/aceiss/okta/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.aceiss.okta.web.rest.vm;
